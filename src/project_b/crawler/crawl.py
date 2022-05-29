5effeb4a3a15f9895b7998530f3a7c1d3af35f85
lmarche <lukas.marche@stud.htwk-leipzig.de>
2022-05-29 11:39:59 +0200

    added apache-any23

--- src/project_b/crawler/crawl.py
+++ src/project_b/crawler/crawl.py
@@ -1,9 +1,6 @@
 import os
 import sys
 
-# any23 must be installed and added to PATH
-# add to project and switch to relativ path?
-
 # example: python3 crawl.py https://www.wikidata.org/wiki/Q10784 test
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -11,6 +8,4 @@ if __name__ == "__main__":
     elif len(sys.argv) < 3:
         print("Missing identifier")
     else:
-        os.system("any23 rover -f ntriples " + sys.argv[1] + "> " + sys.argv[2] + ".nt")
-
-
+        os.system("./apache-any23/bin/any23 rover -f ntriples " + sys.argv[1] + "> " + sys.argv[2] + ".nt")
