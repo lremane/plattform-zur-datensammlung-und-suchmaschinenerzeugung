32c6a778b50a9e71bf3759b673c11c0e8ac91d21
fmergent <56-fmergent@users.noreply.gitlab-softwareprojekt.fim.htwk-leipzig.de>
2022-06-09 16:32:08 +0200

    small refactoring app.py

--- src/crawler/RdfaCrawler.py
+++ src/crawler/RdfaCrawler.py
@@ -1,4 +1,3 @@
-import os
 import requests
 
 
@@ -7,13 +6,9 @@ class RdfaCrawler:
     @staticmethod
     def get_rdfa(site: str) -> str:
 
-        if not os.path.exists("./rdfData"):
-            os.mkdir("./rdfData")
-
         req = requests.get(f"http://www.w3.org/2012/pyRdfa/extract?format=nt&uri={site}")
 
         if req.status_code == 200:
             return req.text
-
         else:
             print("Error")
