d4988e844abc7e457d9537c4f259a5b8a60c33a8
lremane <marclaurintemane@gmail.com>
2022-07-05 00:37:54 +0200

    new qaclient integrated

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
 import os
 import config
 import urllib3
-
 from pyRdfa import pyRdfa
-from QAclient.client import QAClient
+from qaclient import ApiException, Configuration, ApiClient, UserControllerApi, LoginRequest, DatasetControllerKgApi, IndexConfig
 from flask import Flask, render_template, request, jsonify, send_file
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -11,9 +10,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config.update(config.load_config_general().get('flask'))
 
-
-qaclient = QAClient()
 crawler = pyRdfa()
+client_config = Configuration(api_key_prefix={'Authorization': 'Bearer'})
 
 
 @app.route("/")
@@ -29,42 +27,62 @@ def version():
 @app.route('/process_run', methods=['POST'])
 def process_run():
     url = request.get_json()[0]['url']
-    filename = request.get_json()[1]['filename']
+    dataset = request.get_json()[1]['filename']
     result = crawler.rdf_from_source(url, outputFormat='nt')
-    response = qaclient.new_dataset(filename, result)
 
     if result:
         if not os.path.exists("./rdfData"):
             os.mkdir("./rdfData")
-        with open(f"./rdfData/{filename}.nt", "wb") as f:
+        with open(f"./rdfData/{dataset}.nt", "wb") as f:
             f.write(result.encode('utf-8'))
-        return jsonify('ok')
-    elif response == 200:
-        return jsonify('ok')
-    else:
-        return jsonify('Error while processing')
+
+    with ApiClient(client_config) as api_client:
+        api_instance = DatasetControllerKgApi(api_client)
+        file = f'rdfData/{dataset}.nt'
+        index_config = IndexConfig(dataset=dataset)
+
+        try:
+            api_instance.upload_using_post(dataset, file)
+            api_instance.index_using_post(index_config)
+            return jsonify('ok')
+        except ApiException:
+            return jsonify('Error while processing')
+
+
+def upload_dataset():
+    pass
 
 
 @app.route('/check_login_data', methods=['POST'])
 def check_login_data():
-    qaclient.login(request.get_json()[0]['username'],
-                   request.get_json()[1]['password'])
+    with ApiClient(client_config) as api_client:
+        api_instance = UserControllerApi(api_client)
+        login_request = LoginRequest(username_or_email=request.get_json()[0]['username'],
+                                     password=request.get_json()[1]['password'])
 
-    if qaclient.token:
-        return jsonify({'res': '1'})
-    else:
-        return jsonify({'res': '0'})
+        try:
+            auth = api_instance.signin_using_post(login_request)
+            client_config.api_key = {'Authorization': auth.access_token}
+
+            return jsonify({'res': '1'})
+        except ApiException:
+            return jsonify({'res': '0'})
 
 
 @app.route('/crawl-your-data', methods=['POST'])
 def check_login_data_2():
+    with ApiClient(client_config) as api_client:
+        api_instance = UserControllerApi(api_client)
+        login_request = LoginRequest(username_or_email=request.form.get('username'),
+                                     password=request.form.get('password'))
 
-    qaclient.login(request.form.get('username'), request.form.get('password'))
+        try:
+            auth = api_instance.signin_using_post(login_request)
+            client_config.api_key = {'Authorization': auth.access_token}
 
-    if qaclient.token:
-        return render_template('index3.html')
-    else:
-        return render_template('error.html')
+            return render_template('index3.html')
+        except ApiException:
+            return render_template('error.html')
 
 
 @app.route('/data_downloader/<filename>', methods=['POST', 'GET'])
