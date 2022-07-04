bb0b520cb665e45f454a9931f5d3da049329046f
fmergent <56-fmergent@users.noreply.gitlab-softwareprojekt.fim.htwk-leipzig.de>
2022-07-04 16:59:43 +0200

    switched openapi-generator to python package

--- generate client package.sh
+++ generate client package.sh
@@ -1,3 +1,3 @@
 curl https://qanswer-core1.univ-st-etienne.fr/v2/api-docs > api-docs.json
-java -jar openapi-generator-cli.jar generate -g python -i api-docs.json -o qa_client --package-name qa-client --skip-validate-spec
+openapi-generator generate -g python -i api-docs.json -o qa_client --package-name qa-client --skip-validate-spec
 pip install qa_client/
